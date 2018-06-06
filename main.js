0407d4e809023864782bf9223cfed9a57572ea25
Allan Alexandre <alexandesigner@gmail.com>
2018-06-06 11:20:03 -0300

    Removing dependency from package akryum: vue-router2

--- main.js
+++ main.js
@@ -4,8 +4,5 @@ import VueBlazeTemplate from './install';
 
 Vue.use(VueBlazeTemplate);
 
-if (Package['akryum:vue-router'] || Package['akryum:vue-router2']) {
-  import './router-link';
-}
-
+import './router-link';
 import './vue-component';
--- package.js
+++ package.js
@@ -16,10 +16,5 @@ Package.onUse(function(api) {
     'peerlibrary:data-lookup@0.1.0'
   ]);
 
-  api.use([
-    'akryum:vue-router@0.2.2',
-    'akryum:vue-router2@0.1.0'
-  ], {weak: true});
-
   api.mainModule('main.js', 'client');
 });
