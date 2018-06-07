00f18661ec1d9c99586f8510051b0c46fa35395f
alexandesigner <alexandesigner@gmail.com>
2018-06-06 22:57:22 -0300

    Revert code

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'vuejs:blaze-integration',
-  version: '0.3.0',
+  version: '0.2.0',
   summary: "Vue integration with Meteor's Blaze rendering engine."
 });
 
@@ -15,6 +15,11 @@ Package.onUse(function(api) {
     'underscore',
     'peerlibrary:data-lookup@0.1.0'
   ]);
+
+  api.use([
+    'akryum:vue-router@0.2.2',
+    'akryum:vue-router2@0.1.0'
+  ], {weak: true});
 
   api.mainModule('main.js', 'client');
 });
