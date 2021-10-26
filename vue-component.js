b893e889960c331f4923960618655781d3d2745a
Mitar <mitar.git@tnode.com>
2021-10-27 01:45:37 +0200

    Better error message.

--- vue-component.js
+++ vue-component.js
@@ -25,41 +25,45 @@ Template.VueComponent.onRendered(function () {
     // We do not want to recreate the whole component unnecessarily.
     let component = DataLookup.get(() => Template.currentData(this.view), 'component', (a, b) => a === b) || null;
     if (_.isString(component)) {
+      const componentName = component;
       component = Vue.component(component);
+      if (!component) {
+        throw new Error(`Component '${componentName}' not found.`);
+      }
     }
-    else {
+    else if (component) {
       component = Vue.extend(component);
+    } else {
+      throw new Error("Component not provided.");
     }
 
     // Extra arguments to be passed to the component's constructor.
     // Any change in them recreate the whole component.
     let args = DataLookup.get(() => Template.currentData(this.view), 'args', EJSON.equals) || {};
 
-    if (component) {
-      // It can be any element, because it gets replaced by Vue.
-      const el = document.createElement('div');
-      this.view._domrange.parentElement.insertBefore(el, this.view.lastNode());
+    // It can be any element, because it gets replaced by Vue.
+    const el = document.createElement('div');
+    this.view._domrange.parentElement.insertBefore(el, this.view.lastNode());
 
-      // Initial set of non-reactive props.
-      const propsData = Tracker.nonreactive(() => DataLookup.lookup(Template.currentData(this.view), 'props'));
+    // Initial set of non-reactive props.
+    const propsData = Tracker.nonreactive(() => DataLookup.lookup(Template.currentData(this.view), 'props'));
 
-      // To prevent unnecessary reruns of the autorun if constructor registers any dependency.
-      // The only dependency we care about is on "component" which has already been established.
-      this.vm = Tracker.nonreactive(() => {
-        return new component(_.extend({
-          el,
-          propsData,
-        }, args));
+    // To prevent unnecessary reruns of the autorun if constructor registers any dependency.
+    // The only dependency we care about is on "component" which has already been established.
+    this.vm = Tracker.nonreactive(() => {
+      return new component(_.extend({
+        el,
+        propsData,
+      }, args));
+    });
+
+    // And now observe props and update them if they change.
+    this.autorun((computation) => {
+      const props = DataLookup.get(() => Template.currentData(this.view), 'props', EJSON.equals) || {};
+      _.each(_.keys(this.vm._props || {}), (key, i) => {
+        this.vm._props[key] = props[key];
       });
-
-      // And now observe props and update them if they change.
-      this.autorun((computation) => {
-        const props = DataLookup.get(() => Template.currentData(this.view), 'props', EJSON.equals) || {};
-        _.each(_.keys(this.vm._props || {}), (key, i) => {
-          this.vm._props[key] = props[key];
-        });
-      });
-    }
+    });
   });
 });
 
