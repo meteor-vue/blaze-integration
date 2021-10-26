8d3e31b8dc9b560523730089143a773741b3e0f5
Mitar <mitar.git@tnode.com>
2021-10-27 01:53:38 +0200

    Bumping dependencies.

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
   name: 'vuejs:blaze-integration',
-  version: '0.2.0',
+  version: '0.3.0',
   summary: "Vue integration with Meteor's Blaze rendering engine."
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.1');
+  api.versionsFrom('1.8.1');
 
   api.use([
     'ecmascript',
-    'blaze',
-    'templating',
+    'blaze@2.5.0',
+    'templating@1.4.1',
     'ejson',
     'underscore',
-    'peerlibrary:data-lookup@0.1.0'
+    'peerlibrary:data-lookup@0.3.0'
   ]);
 
   api.use([
     'akryum:vue-router@0.2.2',
-    'akryum:vue-router2@0.1.0'
+    'akryum:vue-router2@0.2.3'
   ], {weak: true});
 
   api.mainModule('main.js', 'client');
